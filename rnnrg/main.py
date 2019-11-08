1761c6fa2f9fdc49a8673fde78ab3e70ba237222
Jane Kim <janekim@Janes-MacBook-Pro.local>
2019-11-08 00:13:53 -0500

    reran for layer1

--- rnnrg/main.py
+++ rnnrg/main.py
@@ -2,17 +2,17 @@ from plot import sidebyside_predictions
 
 def main():
 
-    #units_list = [[2**i] for i in range(2, 11)]
-    #sidebyside_predictions(units_list, 'layers1')
+    units_list = [[2**i] for i in range(3, 12)]
+    sidebyside_predictions(units_list, 'layers1')
     
     #fixed_total_units = 128
     #units_list = [[i, fixed_total_units-i] for i in range(8, fixed_total_units, 8)]
     #sidebyside_predictions(units_list, 'layers2')
     
     
-    units_list = [[30,30,120], [30,60,90], [30,90,60], [30,120,30], [60,30,90], \
-                  [60,60,60], [60,90,30], [90,30,60], [90,60,30], [120,30,30]]
-    sidebyside_predictions(units_list, 'layers3')
+    #units_list = [[30,30,120], [30,60,90], [30,90,60], [30,120,30], [60,30,90], \
+    #              [60,60,60], [60,90,30], [90,30,60], [90,60,30], [120,30,30]]
+    #sidebyside_predictions(units_list, 'layers3')
     
     
 
